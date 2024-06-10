306822a164f24ac9d622941d025a724cd91bf462
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-06-10 18:53:50 +0200

    fix: make process responsive for larger screens

--- src/tsx/components/cards/ProcessCard.tsx
+++ src/tsx/components/cards/ProcessCard.tsx
@@ -4,18 +4,53 @@ interface ProcessCardProps {
     label: string;
     shortName: string;
     index: number;
+    activeStep: number;
+    maxIndex: number;
     description: string;
-    isCurrent?: boolean
+    isCurrent?: boolean;
+    isCompleted?: boolean;
 }
 
-const ProcessCard: React.FC<ProcessCardProps> = ({ label, shortName, index, description, isCurrent = false }) => {
+const ProcessCard: React.FC<ProcessCardProps> = ({ label, shortName, index, activeStep, maxIndex, description, isCurrent = false, isCompleted = false }) => {
     return (
-        <div className={`h-full cursor-pointer shadow-md rounded-2xl p-6 border  xl:cursor-default ${isCurrent ? 'bg-green-light-900/10 border-green-light-900' : 'border-grey-100 bg-white'}`}>
-            <span className="text-sm">
-                Schritt {index} von 5: {shortName}
-            </span>
-            <h3 className="my-4 font-lato font-semibold text-lg md:my-5">{label}</h3>
-            <p>{description}</p>
+        <div className="relative">
+            <article className={`h-full p-6 mb-6 cursor-pointer shadow-md rounded-2xl mx-4 border md:mx-6 xl:max-w-[28rem] xl:mx-auto xl:h-auto xl:cursor-default
+                ${isCurrent && !isCompleted ? 'bg-green-light-900/10 border-green-light-900' : ''}
+                ${isCompleted && !isCurrent ? 'border-green-dark-900 bg-white' : ''}
+                ${!isCompleted && !isCurrent ? 'border-grey-100  bg-white' : ''}
+                ${index + 1 === maxIndex ? 'xl:mb-2' : 'xl:mb-0'}`}
+            >
+                <span className="text-sm">
+                    Schritt {index + 1} von {maxIndex}: {shortName}
+                </span>
+                <h3 className="my-4 font-lato font-semibold text-lg lg:my-5">{label}</h3>
+                <p>{description}</p>
+            </article>
+
+            <div className={`relative xl:static xl:before:w-0.5 xl:before:h-1/2 xl:after:w-0.5 xl:after:h-1/2
+                ${index !== 0 ? 'before:w-1/2 before:h-0.5 before:absolute before:left-0 before:top-1/2 before:-z-10' : ''}
+                ${index + 1 < maxIndex ? 'after:w-1/2 after:h-0.5 after:absolute after:right-0 after:top-1/2 after:-z-10 ' : ''}
+                ${index === activeStep ? 'before:bg-green-dark-900 after:bg-grey-100' : ''}
+                ${index < activeStep ? 'before:bg-green-dark-900 after:bg-green-dark-900' : ''}
+                ${index > activeStep ? 'before:bg-grey-100 after:bg-grey-100' : ''}
+                ${index % 2 === 0 ? 'xl:before:-right-10 xl:before:left-auto xl:before:top-0 xl:after:-right-10' : 'xl:before:-left-[2.6rem] xl:before:right-auto xl:before:top-0 xl:after:-left-[2.6rem]'}`}
+            >
+                <figure className={`mx-auto flex items-center justify-between rounded-full w-8 h-8 xl:absolute xl:top-1/2 xl:-translate-y-1/2
+                    ${index === activeStep ? 'text-white bg-green-dark-900' : 'text-white bg-green-light-900'}
+                    ${index < activeStep ? 'border-2 border-green-dark-900 bg-white' : ''}
+                    ${index % 2 === 0 ? 'xl:-right-14' : 'xl:-left-14'}`}
+                >
+                    <span className="block font-semibold font-lato mx-auto">
+                        {index < activeStep ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="#4C7741" className="w-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                            </svg>
+                        ) : (
+                            index + 1
+                        )}
+                    </span>
+                </figure>
+            </div>
         </div>
     );
 };
--- src/tsx/components/sections/Process.tsx
+++ src/tsx/components/sections/Process.tsx
@@ -1,17 +1,14 @@
-import { useState, useEffect, useRef } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { i18nTranslated } from '../../helper/sliderTranslations';
 import '@splidejs/react-splide/css';
 import ProcessCard from "../cards/ProcessCard";
 
 function Process() {
-    const [currentSlide, setCurrentSlide] = useState(0);
     const activeStep = 1;
-    const splideRef = useRef<any>(null);
 
     const steps = [
         {
-            label: "Konzepterstellung & Ideenfindung",
+            label: "Konzepterstellung",
             shortName: "Konzept",
             description: "Occaecat in dolor eiusmod fugiat. Excepteur proident nostrud aliquip esse ut nostrud in ut esse. Proident ut consequat consequat consectetur magna."
         },
@@ -25,26 +22,33 @@ function Process() {
             shortName: "Vorstellung 01",
             description: "Occaecat in dolor eiusmod fugiat. Excepteur proident nostrud aliquip esse ut nostrud in ut esse. Proident ut consequat consequat consectetur magna."
         },
+        {
+            label: "Produktvorstellung 02",
+            shortName: "Vorstellung 02",
+            description: "Occaecat in dolor eiusmod fugiat. Excepteur proident nostrud aliquip esse ut nostrud in ut esse. Proident ut consequat consequat consectetur magna."
+        },
+        {
+            label: "Finale Vorstellung",
+            shortName: "Finalisierung",
+            description: "Occaecat in dolor eiusmod fugiat. Excepteur proident nostrud aliquip esse ut nostrud in ut esse. Proident ut consequat consequat consectetur magna."
+        },
     ];
 
     const breakpoints = {
+        640: {
+            padding: '20%'
+        },
+        1024: {
+            padding: '24%'
+        },
         1280: {
             destroy: true,
         },
     };
 
-    useEffect(() => {
-        if (!splideRef.current) return;
-
-        const splideInstance = splideRef.current.splide;
-        splideInstance.on('move', (newIndex: number) => {
-            setCurrentSlide(newIndex);
-        });
-    }, []);
-
     return (
-        <section className="max-w-208 mx-auto my-28 lg:my-36 xl:my-44 xl:max-w-screen-lg">
-            <article className="px-4 mb-8 md:px-6 lg:mb-14">
+        <section className="my-28 lg:my-36 xl:my-44">
+            <article className="px-4 mb-8 max-w-208 mx-auto md:px-6 lg:mb-14 lg:text-center xl:max-w-screen-lg 2xl:max-w-screen-xl">
                 <h2 className="font-lato font-bold text-2xl mb-6 lg:text-3xl">
                     Der aktuelle Projektfortschritt
                 </h2>
@@ -57,43 +61,24 @@ function Process() {
             </article>
 
             <Splide
-                options={{ rewind: true, arrows: false, pagination: false, i18n: i18nTranslated, mediaQuery: 'min', breakpoints: breakpoints }}
+                options={{ rewind: true, arrows: false, pagination: false, i18n: i18nTranslated, mediaQuery: 'min', breakpoints: breakpoints, start: activeStep }}
                 aria-label="Fakten zum Grünflächenmanagement"
-                className="splide--grid md:px-2"
-                ref={splideRef}
+                className="md:px-2 xl:mx-auto xl:max-w-screen-lg 2xl:max-w-screen-xl"
             >
                 {steps.map((step, index) => (
-                    <SplideSlide key={index} className="pb-10 px-4 lg:px-2 lg:pb-2 xl:first:mb-16 xl:[&:nth-child(2)]:mt-16 xl:[&:nth-child(3)]:-mt-16 xl:[&:nth-child(3)]:mb-16">
+                    <SplideSlide key={index} className="pb-10 xl:w-1/2 xl:even:ml-auto xl:even:mr-0 xl:even:pl-10 xl:pb-0 xl:odd:pr-10">
                         <ProcessCard
                             label={step.label}
                             shortName={step.shortName}
-                            index={index + 1}
+                            index={index}
+                            activeStep={activeStep}
+                            maxIndex={steps.length}
                             isCurrent={index === activeStep}
+                            isCompleted={index < activeStep}
                             description={step.description} />
                     </SplideSlide>
                 ))}
             </Splide>
-
-            <ul className="w-full">
-                {steps.map((step, index) => (
-                    <li key={index} className={`relative before:w-1/2 before:h-0.5 before:absolute before:left-0 before:top-1/2 before:-z-10 after:w-1/2 after:h-0.5 after:absolute after:right-0 after:top-1/2 after:-z-10
-                        ${index === currentSlide ? 'block' : 'hidden'} ${index === activeStep ? 'before:bg-green-dark-900 after:bg-grey-100' : ''} ${index < activeStep ? 'before:bg-green-dark-900 after:bg-green-dark-900' : ''} ${index > activeStep ? 'before:bg-grey-100 after:bg-grey-100' : ''}`}
-                    >
-                        <div className={`mx-auto flex items-center justify-between rounded-full w-8 h-8
-                                ${index === activeStep ? 'text-white bg-green-dark-900' : 'text-white bg-green-light-900'} ${index < activeStep ? 'border-2 border-green-dark-900 bg-white' : ''}`}>
-                            <span className="block font-semibold font-lato mx-auto">
-                                {index < activeStep ? (
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="#4C7741" className="w-4">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
-                                    </svg>
-                                ) : (
-                                    index + 1
-                                )}
-                            </span>
-                        </div>
-                    </li>
-                ))}
-            </ul>
         </section>
     );
 }
