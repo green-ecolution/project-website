c4e50edee73903bac6e1a239a417b14a587b182c
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-06-14 19:37:31 +0200

    fix: add devider (wip)

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -3,11 +3,13 @@ import HompageHero from "./components/sections/HomepageHero";
 import Introduction from "./components/sections/Introduction";
 import Process from "./components/sections/Process";
 import Stakeholder from "./components/sections/Stakeholder";
+import StartpageDevider from "./components/sections/StartpageDevider";
 
 function App() {
   return (
     <div>
         <HompageHero />
+        <StartpageDevider />
         <Introduction />
         <Process />
         <Stakeholder />
