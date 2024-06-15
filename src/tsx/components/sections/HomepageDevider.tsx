1811f379606a7a9a49dd94a77b2970a986047338
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-06-15 10:33:00 +0200

    feat: add third tree

--- src/tsx/components/sections/HomepageDevider.tsx
+++ src/tsx/components/sections/HomepageDevider.tsx
@@ -1,6 +1,6 @@
 function HomepageDevider() {
     return (
-        <figure className="overflow-hidden absolute -left-[50%] -bottom-80 sm:-bottom-80 sm:-left-[20%] md:-left-[10%] 2xl:left-auto">
+        <figure className="overflow-hidden absolute -left-[50%] -z-10 -bottom-80 sm:-bottom-80 sm:-left-[20%] md:-left-[10%] 2xl:left-auto">
             <svg className="w-[140%] sm:block sm:w-[140%] md:w-[120%] 2xl:w-screen" aria-hidden="true" width="1704" height="586" viewBox="0 0 1704 586" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1031.72 76.1851C706.589 186.685 283.923 32.185 283.923 32.185C283.923 32.185 38.9638 -57.8149 9.92366 63.1851C-26.0765 213.185 45.7064 407.588 238.265 386C546 351.5 655.924 301.185 1024.92 456.685C1282.92 565.409 1404.11 583.147 1475.42 585.685C1615.92 590.685 1710.41 504.185 1702.92 415.685C1695.43 327.185 1605.42 233.685 1564.92 198.185C1524.42 162.685 1303.54 -16.1987 1031.72 76.1851Z" fill="#4C7741"/>
             </svg>
