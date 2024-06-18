c4f7f57a1345d235dc549ff6bbdaf7e4676ba21b
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-06-18 15:38:53 +0200

    feat: add animations to icons

--- src/tsx/components/homepage/HomepageIcons.tsx
+++ src/tsx/components/homepage/HomepageIcons.tsx
@@ -1,35 +1,72 @@
-function HomepageIcons() {
+interface HomepageIconsProps {
+    index: number;
+}
+
+const HomepageIcons: React.FC<HomepageIconsProps> = ({ index }) => {
+    const generalClasses = "absolute rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:rounded-full";
+
     return (
         <div>
-            <figure aria-hidden="true" className="absolute top-[20%] left-[10%] bg-white w-28 h-28 rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:w-36 before:h-36 before:rounded-full">
+            <figure
+                aria-hidden="true"
+                className={`top-[20%] left-[10%] w-28 h-28  before:w-36 before:h-36
+                    ${generalClasses}
+                    ${index == 1 ? 'bg-white before:animate-pulse' : ''}`}
+            >
                 <img
                     src="/assets/svg/general/wlan.svg"
                     className="object-contain w-14" alt="" />
             </figure>
 
-            <figure aria-hidden="true" className="absolute top-[35%] left-[30%] bg-white w-28 h-28 rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:w-36 before:h-36 before:rounded-full">
+            <figure
+                aria-hidden="true"
+                className={`top-[35%] left-[30%] w-28 h-28 before:w-36 before:h-36
+                    ${generalClasses}
+                    ${index == 1 || index == 2 ? 'bg-white before:animate-pulse' : ''}`}
+            >
                 <img
                     src="/assets/svg/general/mobile.svg"
                     className="object-contain h-12 w-12" alt="" />
             </figure>
 
-            <figure aria-hidden="true" className="absolute bottom-[10rem] left-[10%] bg-white w-28 h-28 rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:w-36 before:h-36 before:rounded-full">
+            <figure
+                aria-hidden="true"
+                className={`bottom-[10rem] left-[10%] w-28 h-28 ro before:w-36 before:h-36
+                    ${generalClasses}
+                    ${index == 1 ? 'bg-white before:animate-pulse' : ''}`}>
                 <img
                     src="/assets/svg/general/gateway.svg"
                     className="object-contain w-14" alt="" />
             </figure>
 
-            <figure aria-hidden="true" className="absolute bottom-[5rem] right-1/2 bg-white w-28 h-28 rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:w-36 before:h-36 before:rounded-full">
+            <figure
+                aria-hidden="true"
+                className={`bottom-[5rem] right-1/2 w-28 h-28  before:w-36 before:h-36
+                    ${generalClasses}
+                    ${index == 0 ? 'bg-white before:animate-pulse' : ''}`}
+            >
                 <img
                     src="/assets/svg/general/tensiometer.svg"
                     className="object-contain h-14 w-14" alt="" />
             </figure>
-            <figure aria-hidden="true" className="absolute bottom-[6rem] right-[26%] bg-white w-20 h-20 rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:w-28 before:h-28 before:rounded-full">
+
+            <figure
+                aria-hidden="true"
+                className={`bottom-[6rem] right-[26%] w-20 h-20 before:w-28 before:h-28 2xl:right-[32%]"
+                    ${generalClasses}
+                    ${index == 0 ? 'bg-white before:animate-pulse' : ''}`}
+            >
                 <img
                     src="/assets/svg/general/tensiometer.svg"
                     className="object-contain h-12 w-12" alt="" />
             </figure>
-            <figure aria-hidden="true" className="absolute bottom-[6rem] right-[10%] bg-white w-16 h-16 rounded-full flex items-center justify-center before:bg-white/30 before:absolute before:-z-10 before:w-[5.5rem] before:h-[5.5rem] before:rounded-full">
+
+            <figure
+                aria-hidden="true"
+                className={`bottom-[6rem] right-[10%] w-16 h-16 before:w-[5.5rem] before:h-[5.5rem] 2xl:right-[20%]"
+                    ${generalClasses}
+                    ${index == 0 ? 'bg-white before:animate-pulse' : ''}`}
+            >
                 <img
                     src="/assets/svg/general/tensiometer.svg"
                     className="object-contain h-10 w-10" alt="" />
