b550ea3c6146edec746f715f5498917ba3e268d6
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-06-23 10:39:09 +0200

    feat: add background to stakeholder and contact section

--- src/tsx/pages/HomePage.tsx
+++ src/tsx/pages/HomePage.tsx
@@ -13,9 +13,11 @@ function HomePage() {
         <HomepageDevider />
         <Introduction />
         <Process />
-        <Stakeholder />
-        <Faq />
-        <Contact />
+        <div className="relative overflow-x-hidden before:bg-cover before:bg-background-light-dot before:w-[90%] before:h-[120vh] before:absolute before:-right-4 before:top-8 before:-z-10 before:bg-no-repeat sm:before:-right-[5%] xl:before:w-[90rem] 2xl:before:right-[10%] 2xl:before:bg-contain 3xl:before:right-[20%]">
+            <Stakeholder />
+            <Faq />
+        </div>
+        <Contact spacingTop={false}/>
     </main>
   );
 }
