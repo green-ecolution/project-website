b431c7833f2a94c615df9e822505fb14ed9d35ce
Linus <linus.bumann@stud.hs-flensburg.de>
2024-06-27 11:25:35 +0200

    Changed Text in DashboardPreview.tsx

--- src/tsx/components/sections/DashboardPreview.tsx
+++ src/tsx/components/sections/DashboardPreview.tsx
@@ -1,25 +1,30 @@
 import Lottie from "lottie-react";
-import dashboardAnimation from "../../../json/dashboardAnimation.json"
+import dashboardAnimation from "../../../json/dashboardAnimation.json";
 
 function DashboardPreview() {
-  return (
-    <section className="my-28 px-4 max-w-208 mx-auto md:px-6 lg:my-36 xl:max-w-screen-lg xl:my-52">
-        <article className="mb-8 g:mb-14 md:text-center">
-            <h2 className="font-lato font-bold text-2xl mb-6 lg:text-3xl">
-                Fundierte Handlungsempfehlungen<br/>
-                zur Bewässerung auf einen Blick:
-            </h2>
-            <p>
-                Unser System liefert durch die wissenschaftliche Datenauswertung fundierte Handlungsempfehlungen, wann welche Vegetation an welchem Standort
-                werden muss. Excepteur do consectetur quis nulla ut et nisi aute id consectetur excepteur nostrud anim culpa reprehenderit.
-            </p>
-        </article>
+    return (
+        <section className="my-28 px-4 max-w-208 mx-auto md:px-6 lg:my-36 xl:max-w-screen-lg xl:my-52">
+            <article className="mb-8 g:mb-14 md:text-center">
+                <h2 className="font-lato font-bold text-2xl mb-6 lg:text-3xl">
+                    Fundierte Handlungsempfehlungen
+                    <br />
+                    zur Bewässerung auf einen Blick
+                </h2>
+                <p>
+                    Unser System liefert anhand wissenschaftlicher
+                    Datenauswertung fundierte Handlungsempfehlungen, zu welchem
+                    Zeitpunkt eine Bewässerung notwendig ist. Die Daten werden
+                    dahingehend aufbereitet, dass diese einfach zu
+                    interpretieren sind und in die Einsatzplanung eingebunden
+                    werden können.
+                </p>
+            </article>
 
-        <figure className="translate-x-4 xl:translate-x-16">
-            <Lottie animationData={dashboardAnimation} />
-        </figure>
-    </section>
-  );
+            <figure className="translate-x-4 xl:translate-x-16">
+                <Lottie animationData={dashboardAnimation} />
+            </figure>
+        </section>
+    );
 }
 
 export default DashboardPreview;
