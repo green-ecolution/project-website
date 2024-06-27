66ba19ded335bcea5dec9efd861fc225ac856246
Linus <linus.bumann@stud.hs-flensburg.de>
2024-06-27 09:53:12 +0200

    Changed Text in NotFoundPage.tsx

--- src/tsx/pages/NotFoundPage.tsx
+++ src/tsx/pages/NotFoundPage.tsx
@@ -1,8 +1,7 @@
 import Lottie from "lottie-react";
-import cableAnimation from "../../json/cableAnimation.json"
+import cableAnimation from "../../json/cableAnimation.json";
 import Button from "../components/Button";
 import Arrow from "../icons/Arrow";
-
 
 function NotFoundPage() {
     return (
@@ -14,16 +13,17 @@ function NotFoundPage() {
             <div className="mt-[55vh] mx-auto max-w-208 xl:max-w-screen-lg">
                 <section className="my-28 px-4 md:px-6 lg:my-36 xl:my-52">
                     <h1 className="font-lato font-bold text-4xl mb-4 lg:mb-6 lg:text-5xl lg:text-center xl:text-6xl">
-                        Die Seite konnte leider nicht gefunden werden.
+                        Die Seite konnte nicht gefunden werden.
                     </h1>
                     <p className="lg:text-center mb-10">
-                        Eu elit quis eiusmod proident officia aute tempor tempor qui commodo aute qui.
-                        Excepteur id ea laboris fugiat dolor exercitation ut pariatur ut commodo non.
+                        Die gewünschte Seite ist nicht erreichbar, da sie
+                        entweder nicht exisitert oder es zu einem Fehler
+                        gekommen ist. Error-Code: 404
                     </p>
                     <div className="lg:flex lg:items-center lg:justify-center">
                         <Button href="/" ariaLabel="Zur Statseite" isDark>
                             <span>Zur Startseite</span>
-                            <Arrow classes="w-6 text-white transition-all ease-in-out duration-300 group-hover:translate-x-2"/>
+                            <Arrow classes="w-6 text-white transition-all ease-in-out duration-300 group-hover:translate-x-2" />
                         </Button>
                     </div>
                 </section>
