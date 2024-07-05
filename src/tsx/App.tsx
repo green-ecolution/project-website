4e5cb544765aea55bc6709a2bbf7d60f10c03303
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-07-05 08:41:27 +0200

    feat: add og-image

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -5,16 +5,22 @@ import NotFoundPage from "./pages/NotFoundPage";
 import ContactPage from "./pages/ContactPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="projekt" element={<ProjectPage />} />
-        <Route path="kontakt" element={<ContactPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    const ogImage = document.getElementById('og-image');
+    if (ogImage) {
+        const currentDomain = window.location.origin;
+        ogImage.setAttribute('content', currentDomain + '/public/assets/images/open-graph-image.png');
+    }
+
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="projekt" element={<ProjectPage />} />
+            <Route path="kontakt" element={<ContactPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
