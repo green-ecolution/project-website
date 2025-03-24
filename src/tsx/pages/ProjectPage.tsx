fac2256a050db3b95b4014bbc064d73d736e4f9f
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2025-03-24 10:29:29 +0100

    chore: update hero content

--- src/tsx/pages/ProjectPage.tsx
+++ src/tsx/pages/ProjectPage.tsx
@@ -26,8 +26,8 @@ function ProjectPage() {
         "Das Projekt Green Ecolution beschäftigt sich mit der Frage, wie man die Bewässerung von Bäumen effizienter umsetzen kann. \
         In Zusammenarbeit mit dem TBZ-Flensburg werden mehrere Bäume an verschiedenen Standorten mit Sensorik ausgestattet, \
         um den Wasserbedarf von Bäumen zu ermitteln und entsprechend anhand der Daten Handlungsempfehlungen auszusprechen, um diese \
-        in der Einsatzplanung mit zu berücksichtigen. Dabei beschränkt sich das Projekt nicht nur auf die Stadt, sondern auch auf die Bürger:innen. \
-        Über eine Übersichtskarte erhalten die Bürger:innen Informationen über den Stand der Bewässerung.";
+        in der Einsatzplanung mit zu berücksichtigen. Dabei wird die Vereinfachung und die Digitalisierung der Einsatzplanung umgesetzt, \
+        inklusive einer dynamischen Routenplanung, um die Bewässerung noch gezielter und ressourcenschonender umzusetzen.";
 
     return (
         <main className="relative overflow-hidden before:bg-cover before:bg-background-light-dot before:w-4/5 before:h-[100vh] before:max-h-[45rem] before:absolute before:-right-4 before:-top-16 before:-z-10 before:bg-no-repeat sm:before:-right-10 lg:before:max-h-[55rem] xl:before:w-[70rem] xl:before:-right-40 2xl:before:right-[10%] 2xl:before:bg-contain">
