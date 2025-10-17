a50c3be26bbf7928dddf4963bccebf637f0eb2d9
Cedrik Hoffmann <choffmann@progeek.de>
2025-10-17 09:12:26 +0200

    feat: add new stakeholder progeek

--- src/tsx/components/sections/Stakeholder.tsx
+++ src/tsx/components/sections/Stakeholder.tsx
@@ -10,27 +10,32 @@ interface StakeholderProps {
 const Stakeholder: React.FC<StakeholderProps> = ({ hasDesktopList = false }) => {
   const stakeholder = [
     {
-      label: 'Hochschule Flensburg',
-      url: 'https://hs-flensburg.de/',
-      image: '/assets/images/logo/hochschule-flensburg.png',
+      label: 'PROGEEK GmbH',
+      url: 'https://progeek.de/',
+      image: '/assets/images/logo/progeek.svg',
       description:
-        'Die Hochschule-Flensburg bietet praxisnahe Studiengänge mit Fokus auf angewandte Forschung und enge Zusammenarbeit mit der Industrie an.',
+        'PROGEEK steht für innovative IT-Lösungen, individuelle Softwareentwicklung und digitale Beratung – mit dem Ziel, Unternehmen auf ihrem Weg in die Zukunft der Digitalisierung zu begleiten.',
     },
     {
       label: 'Smarte Grenzregion',
       url: 'https://smarte-grenzregion.de/',
       image: '/assets/images/logo/smarte-grenzregion.png',
       description:
-        'Das Projekt „Smarte Grenzregion zwischen den Meeren“ will die digitale Transformation in der Region vorantreiben, \
-                 um eine integrierte und nachhaltige Stadt- und Regionalentwicklung zu gestalten und das Gemeinwohl zu stärken.',
+        'Das Projekt „Smarte Grenzregion zwischen den Meeren“ will die digitale Transformation in der Region vorantreiben, um eine integrierte und nachhaltige Stadt- und Regionalentwicklung zu gestalten und das Gemeinwohl zu stärken.',
+    },
+    {
+      label: 'Hochschule Flensburg',
+      url: 'https://hs-flensburg.de/',
+      image: '/assets/images/logo/hochschule-flensburg.png',
+      description:
+        'Die Hochschule Flensburg bietet praxisnahe Studiengänge mit Fokus auf angewandte Forschung und enge Zusammenarbeit mit der Industrie an.',
     },
     {
       label: 'TBZ Flensburg',
       url: 'https://www.tbz-flensburg.de/',
       image: '/assets/images/logo/tbz.png',
       description:
-        'Das Technische Betriebszentrum (TBZ) ist der zentrale Dienstleister für die Stadt Flensburg und u.a. \
-                für die Pflege öffnetlicher Grünflächen, Straßenreinigung sowie Abfallwirtschaft zuständig.',
+        'Das Technische Betriebszentrum (TBZ) ist der zentrale Dienstleister für die Stadt Flensburg und u.a. für die Pflege öffnetlicher Grünflächen, Straßenreinigung sowie Abfallwirtschaft zuständig.',
     },
   ]
 
@@ -49,9 +54,9 @@ const Stakeholder: React.FC<StakeholderProps> = ({ hasDesktopList = false }) => 
       <article className={`px-4 mb-8 md:px-6 lg:mb-14 ${hasDesktopList ? 'lg:text-center' : ''}`}>
         <h2 className="font-lato font-bold text-2xl mb-6 lg:text-3xl">Wer sind die Beteiligten?</h2>
         <p>
-          In Zusammenarbeit zwischen der Hochschule-Flensburg, der Smarten-Grenzregion und dem
-          TBZ-Flensburg soll in der Stadt Flensburg eine smarte Bewässerung für Bäume aufgebaut
-          werden.
+          In Zusammenarbeit zwischen PROGEEK, der Smarten-Grenzregion, der Stadt Flensburg und der
+          Hochschule Flensburg soll eine bedarfsgerechte und datenbasierte Bewässerung für Bäume
+          aufgebaut werden.
         </p>
       </article>
 
