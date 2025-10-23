42e3188fe4943b2ca594154089ad683b40148630
Cedrik Hoffmann <dev@choffmann.io>
2025-10-23 14:30:50 +0200

    feat: update hero description in project page

--- src/tsx/pages/ProjectPage.tsx
+++ src/tsx/pages/ProjectPage.tsx
@@ -23,11 +23,11 @@ function ProjectPage() {
 
   const heroHeadline = 'Alles wissenswerte über das Projekt'
   const heroDescription =
-    'Das Projekt Green Ecolution beschäftigt sich mit der Frage, wie man die Bewässerung von Bäumen effizienter umsetzen kann. \
-        In Zusammenarbeit mit dem TBZ-Flensburg werden mehrere Bäume an verschiedenen Standorten mit Sensorik ausgestattet, \
-        um den Wasserbedarf von Bäumen zu ermitteln und entsprechend anhand der Daten Handlungsempfehlungen auszusprechen, um diese \
-        in der Einsatzplanung mit zu berücksichtigen. Dabei wird die Vereinfachung und die Digitalisierung der Einsatzplanung umgesetzt, \
-        inklusive einer dynamischen Routenplanung, um die Bewässerung noch gezielter und ressourcenschonender umzusetzen.'
+    'Green Ecolution befasst sich mit der Frage, wie die Bewässerung von Bäumen effizienter und nachhaltiger gestaltet werden kann. \
+  In Zusammenarbeit mit dem TBZ Flensburg wurden im Rahmen eines Forschungsprojekts verschiedene Standorte mit Sensorik ausgestattet, \
+  um den Wasserbedarf von Bäumen präzise zu erfassen. Aufbauend auf diesen Erkenntnissen entwickeln wir das System nun weiter, \
+  um datenbasierte Handlungsempfehlungen und eine digitalisierte Einsatzplanung mit dynamischer Routenoptimierung zu ermöglichen – \
+  für eine gezielte und ressourcenschonende Bewässerung im urbanen Raum.'
 
   return (
     <main className="relative overflow-hidden before:bg-cover before:bg-background-light-dot before:w-4/5 before:h-[100vh] before:max-h-[45rem] before:absolute before:-right-4 before:-top-16 before:-z-10 before:bg-no-repeat sm:before:-right-10 lg:before:max-h-[55rem] xl:before:w-[70rem] xl:before:-right-40 2xl:before:right-[10%] 2xl:before:bg-contain">
