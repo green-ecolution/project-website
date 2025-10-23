f14cbf3324845e05e561a17a679379209645f4cd
Cedrik Hoffmann <dev@choffmann.io>
2025-10-23 14:30:02 +0200

    feat: update conteact hero description

--- src/tsx/pages/ContactPage.tsx
+++ src/tsx/pages/ContactPage.tsx
@@ -10,7 +10,7 @@ function ContactPage() {
 
   const heroHeadline = 'Weitere Informationen'
   const heroDescription =
-    'Bei weiteren Rückfragen können Sie uns über unsere E-Mail-Adresse erreichen. Eine Übersicht über die technische Struktur erhalten Sie in unserem öffentlich zugänglichen GitHub-Projekt.'
+    'Wenn du weitere Fragen hast, erreichst du uns per E-Mail. Einen Einblick in die technische Struktur bekommst du in unserem öffentlich zugänglichen GitHub-Projekt.'
 
   return (
     <main className="relative overflow-hidden before:bg-cover before:bg-background-dark-dot before:w-4/5 before:h-[100vh] before:max-h-[45rem] before:absolute before:-right-4 before:-top-16 before:-z-10 before:bg-no-repeat sm:before:-right-10 lg:before:max-h-[55rem] xl:before:w-[70rem] xl:before:-right-40 2xl:before:right-[10%] 2xl:before:bg-contain">
