de2ac280ed51de0595eae83e898615cf3602d988
Cedrik Hoffmann <dev@choffmann.io>
2025-10-24 12:37:42 +0200

    feat: display version

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+declare global {
+  interface ImportMetaEnv {
+    readonly VITE_APP_VERSION: string
+    readonly VITE_BUILD_VERSION: string
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+}
+
+export {}
