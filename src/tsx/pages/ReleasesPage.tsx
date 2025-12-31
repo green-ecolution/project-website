2274e9eac392a8aeed8be4ccabe7a785ab8bb6c4
Cedrik Hoffmann <dev@choffmann.io>
2025-12-31 01:57:42 +0100

    fix: ensure footer stays at bottom with flex layout

--- src/tsx/pages/ReleasesPage.tsx
+++ src/tsx/pages/ReleasesPage.tsx
@@ -10,7 +10,7 @@ function ReleasesPage() {
   }, [])
 
   return (
-    <main className="relative overflow-hidden before:bg-cover before:bg-background-light-dot before:w-4/5 before:h-[100vh] before:max-h-[45rem] before:absolute before:-right-4 before:-top-16 before:-z-10 before:bg-no-repeat sm:before:-right-10 lg:before:max-h-[55rem] xl:before:w-[70rem] xl:before:-right-40 2xl:before:right-[10%] 2xl:before:bg-contain">
+    <main className="relative overflow-hidden flex-grow before:bg-cover before:bg-background-light-dot before:w-4/5 before:h-[100vh] before:max-h-[45rem] before:absolute before:-right-4 before:-top-16 before:-z-10 before:bg-no-repeat sm:before:-right-10 lg:before:max-h-[55rem] xl:before:w-[70rem] xl:before:-right-40 2xl:before:right-[10%] 2xl:before:bg-contain">
       <section className="px-4 max-w-208 mx-auto pt-8 pb-16 md:px-6 lg:pt-16 lg:pb-24 lg:max-w-screen-lg xl:max-w-screen-xl">
         <h1 className="font-lato font-bold text-3xl mb-4 lg:text-4xl">
           Releases
